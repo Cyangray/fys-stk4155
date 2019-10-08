66a41ffed8d01affc0fddeb8caf0f3b2e34d5e38
loopdiloop <mamoll@uio.no>
2019-10-08 12:58:09 +0200

    Mostly comments.

--- run_g.py
+++ run_g.py
@@ -7,7 +7,7 @@ from functions import reduce4
 
 from data_generation import data_generate
 from fit_matrix import fit
-from visualization import plot_3d, plot_3d_terrain, plot_bias_var_tradeoff, plot_mse_vs_complexity
+from visualization import plot_3d, plot_3d_terrain, plot_bias_var_tradeoff, plot_mse_vs_complexity, plot_cmap
 import statistical_functions as statistics
 from sampling_methods import sampling
 
@@ -52,8 +52,8 @@ rescaled_dataset = dataset.rescale_back(z = z_model_norm)
 z_model = rescaled_dataset[2]
 
 #plot_3d_terrain(dataset.x_unscaled, dataset.y_unscaled, z_model)
-plot_3d_terrain(rescaled_dataset[0], rescaled_dataset[1], rescaled_dataset[2], dataset.x_unscaled, dataset.y_unscaled, dataset.z_unscaled)
-
+#plot_3d_terrain(rescaled_dataset[0], rescaled_dataset[1], rescaled_dataset[2], dataset.x_unscaled, dataset.y_unscaled, dataset.z_unscaled)
+plot_cmap(rescaled_dataset[0], rescaled_dataset[1], rescaled_dataset[2], dataset.x_unscaled, dataset.y_unscaled, dataset.z_unscaled)
 
 
 
