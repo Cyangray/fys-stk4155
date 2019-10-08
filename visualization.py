1515c507a617f8051c15192e2ece643e29477097
Marianne Bjerke <mamoll@uio.no>
2019-10-08 19:26:59 +0200

    Clean up and adding of comments

--- visualization.py
+++ visualization.py
@@ -9,20 +9,11 @@ def plot_3d(x, y, z, an_x, an_y, an_z, plot_type):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.set_title("The franke function model and analytical solution.", fontsize=22)
-
-    #surf = ax.plot_surface(x_array, y_array, z_tilde, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
+    
     # Surface of analytical solution.
     surf = ax.plot_surface(an_x, an_y, an_z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
-
     surf_2 = ax.scatter(x, y, z)
-    """if plot_type[i] == "surface":
-        surf = ax.plot_surface(x[i], y[i], Z[i], cmap=cm.coolwarm, linewidth=0, antialiased=False)
-    elif plot_type[i] == "scatter":
-        surf_2 = ax.scatter(x[i], y[i], Z[i])
-    else:
-        AssertionError("error")"""
+
     # Customize the z axis.
     ax.set_zlim(-0.30, 2.40)#(-0.10, 1.40)
     ax.zaxis.set_major_locator(LinearLocator(10))
@@ -31,8 +22,7 @@ def plot_3d(x, y, z, an_x, an_y, an_z, plot_type):
     ax.set_ylabel("y")
 
     # Add a color bar which maps values to colors.
-    #fig.colorbar(surf, shrink=0.5, aspect=5)
-
+    fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
         
         
@@ -42,29 +32,14 @@ def plot_3d_terrain(x, y, z, x_map, y_map, z_map):
     ax = fig.gca(projection='3d')
     
     surf1 = ax.plot_trisurf(x_map, y_map, z_map, cmap=cm.coolwarm, alpha=0.2)
-    
     surf2 = ax.scatter(x, y, z)
     
-    # Customize the z axis.
-    #ax.set_zlim(-0.30, 2.40)#(-0.10, 1.40)
-    #ax.zaxis.set_major_locator(LinearLocator(10))
-    #ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
     ax.set_xlabel("x")
     ax.set_ylabel("y")
 
     # Add a color bar which maps values to colors.
-    #fig.colorbar(surf, shrink=0.5, aspect=5)
-
+    fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
-
-def plot_cmap(x, y, z, x_map, y_map, z_map):
-    f, ax = plt.subplots(1,2, sharex=True, sharey=True)
-    ax[0].tripcolor(x,y,z)
-    ax[1].tricontourf(x,y,z, 20) # choose 20 contour levels, just to show how good its interpolation is
-    ax[1].plot(x,y, 'ko ')
-    ax[0].plot(x,y, 'ko ')
-    plt.show()
-    plt.savefig('test.png')
 
 def plot_bias_var_tradeoff(deg, mse):
     """ Plots bias-variance tradeoff for different polynoial degrees of models. """
